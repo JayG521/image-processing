8aef2219a35bf947011251bab1227ce74fdefde1
JayG521 <104608922+JayG521@users.noreply.github.com>
2024-02-16 17:11:20 +1300

    update core #3

--- scr core.py
+++ scr core.py
@@ -1,4 +1,5 @@
 ## image processing
+import PIL
 from PIL import Image
 
 rope = [50, 22, 22, 22, 2, 10, 7]
@@ -13,3 +14,4 @@ pick = Image.open("lab-test-page.jpg")
 
 print(total)
 ## preview
+pick.draft("L", (pick.width // 2, pick.height // 2))
