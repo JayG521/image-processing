43ca42c3b4cdd2d051496935d1eef544d9ec07ca
JayG521 <104608922+JayG521@users.noreply.github.com>
2024-04-26 17:24:13 +1200

    Update core

--- scr core.py
+++ scr core.py
@@ -17,19 +17,19 @@ canvas = [199+emulsion, 190+emulsion, 200+emulsion]
 heavyCotton = [240+emulsion, 230+emulsion, 206+emulsion]
 artPaper = [179+emulsion, 180+emulsion, 177+emulsion]
 
-
 ## import image
 pick = Image.open("lab-test-page.jpg")
 
 
 ## image object
+pick.effect_spread(4)
+nSize = (300, 190)
+pick = pick.resize(nSize)
 
 
 # print(total)
+##outputs
 pick.show()
-learntest = pick.getcolors()
-print(learntest)
 
-
+pick.save("lab-test-page-convert-test.png")
 ## preview
-# pick.draft("L", (pick.width // 2, pick.height // 2))
